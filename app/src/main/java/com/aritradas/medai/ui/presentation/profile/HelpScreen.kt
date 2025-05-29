bb50ae29512b5bea7f8934d2a11dfe1c983386aa
aritra-tech <aritrarick2002@gmail.com>
2025-05-29 22:23:25 +0530

    feat: Implement Help Screen UI

--- app/src/main/java/com/aritradas/medai/ui/presentation/profile/HelpScreen.kt
+++ app/src/main/java/com/aritradas/medai/ui/presentation/profile/HelpScreen.kt
@@ -1,8 +1,12 @@
 package com.aritradas.medai.ui.presentation.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +18,12 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aritradas.medai.R
+import com.aritradas.medai.ui.presentation.profile.components.SettingsCard
+import com.aritradas.medai.ui.presentation.profile.components.SettingsItemGroup
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,6 +58,40 @@ fun HelpScreen(
             )
         }
     ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp),
+        ) {
+            SettingsItemGroup {
+                SettingsCard (
+                    itemName = stringResource(R.string.send_feedback),
+                    onClick = {
+                    }
+                )
 
+                HorizontalDivider(
+                    thickness = 1.dp
+                )
+
+                SettingsCard(
+                    itemName = stringResource(R.string.terms_conditions),
+                    onClick = {
+                    }
+                )
+
+                HorizontalDivider(
+                    thickness = 1.dp
+                )
+
+                SettingsCard(
+                    itemName = stringResource(R.string.privacy_policy),
+                    onClick = {
+                    }
+                )
+            }
+
+        }
     }
 }
