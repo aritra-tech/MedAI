f0c46531228595bb9d36637a62afd038d98f3017
aritra-tech <aritrarick2002@gmail.com>
2025-06-25 09:17:26 +0530

    Increment versionCode to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.aritradas.medai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
