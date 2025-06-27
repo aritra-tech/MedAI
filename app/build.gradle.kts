60592fdd5650b817aaadec275cce2c3d1848bb83
aritra-tech <aritrarick2002@gmail.com>
2025-06-27 21:26:34 +0530

    fix: the icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.aritradas.medai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.0"
+        versionCode = 9
+        versionName = "1.0.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
